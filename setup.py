6a04f1d60a5777df27817ea26a46d5e49a1355fc
Andrea Pollastro <andrea.poll93@gmail.com>
2025-04-04 15:39:10 +0200

    Bump version to 1.0.9 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="epics-archiver-tools",
-    version="1.0.8",
+    version="1.0.9",
     author="Andrea Pollastro",
     author_email="apollastro@lbl.gov",
     description="A Python package for interacting with EPICS archiver data",
