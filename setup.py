632a157bf82fa0758c83337809793dff98b6c68e
Andrea Pollastro <andrea.poll93@gmail.com>
2025-04-04 13:36:39 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().splitlines()
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="archivertools",
+    name="epics-archiver-tools",
     version="1.0.0",
     author="Andrea Pollastro",
     author_email="apollastro@lbl.gov",
@@ -16,7 +13,16 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/andrea-pollastro/archivertools",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        "numpy>=1.26.3",
+        "pandas>=2.1.4",
+        "python-dateutil>=2.8.2",
+        "pytz>=2023.3",
+        "six>=1.16.0",
+        "tzdata>=2023.3",
+        "urllib3>=2.0.0",
+        "tqdm>=4.65.0",
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
